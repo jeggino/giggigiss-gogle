45fa131b13104e153b20c2c9d6ec8dc88cc49d9a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-14 09:41:12 +0200

    Create home.py

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+from streamlit_option_menu import option_menu  
+from datetime import datetime as dt
+import random
+from dateutil import parser
+
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+df_old = conn.read(ttl=0,worksheet="Data")
