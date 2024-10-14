ca8393a4ed5a760bbfe2c7d1f3e2220ccdf652d5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-14 16:49:06 +0200

    Update home.py

--- home.py
+++ home.py
@@ -25,7 +25,7 @@ st.set_page_config(
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 df_observations = conn.read(ttl=0,worksheet="df_observations")
-df_references = conn.read(ttl=660,worksheet="df_users")
+df_references = conn.read(ttl='660',worksheet="df_users")
 
 df_point = df_observations
 df_references = df_references
