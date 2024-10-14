4251970b11b28b935cd5298d66361d467d313b62
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-14 10:44:33 +0200

    Update home.py

--- home.py
+++ home.py
@@ -11,3 +11,34 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 df_old = conn.read(ttl=0,worksheet="df_observations")
 
 df_old
+
+
+import streamlit as st
+import pydeck
+import pandas as pd
+
+
+point_layer = pydeck.Layer(
+    "ScatterplotLayer",
+    data=df_old,
+    id="id",
+    get_position=["lng", "lat"],
+    get_color="[255, 75, 75]",
+    pickable=True,
+    auto_highlight=True,
+    get_radius="aantal",
+)
+
+view_state = pydeck.ViewState(
+    latitude=df_old.lat.mean(), longitude=df_old.lng.mean(), controller=True, zoom=2.4, pitch=30
+)
+
+chart = pydeck.Deck(
+    point_layer,
+    initial_view_state=view_state,
+    tooltip={"text": "{project}"},
+)
+
+event = st.pydeck_chart(chart, on_select="rerun", selection_mode="multi-object")
+
+event.selection
