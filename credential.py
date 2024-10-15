61525b6603fc90e9647add128545b7934f6f175a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-15 23:24:27 +0200

    Update credential.py

--- credential.py
+++ credential.py
@@ -113,4 +113,5 @@ DICTIONARY_USERS = {"Luigi": ["Niet gespecificeerd"],
 
 DICTIONARY_PROJECTS = {"Overig":["Vogels","Vleermuizen","Vleermuiskast"],
                       "Ratten Terschelling":["Camera","Rat val",'Vangkooi'],
-                      "Admin":["Vogels","Vleermuizen","Vleermuiskast","Camera","Rat val",'Vangkooi']}
+                      "Admin":["Vogels","Vleermuizen","Vleermuiskast","Camera","Rat val",'Vangkooi'],
+                       "Bats balkans":["Vleermuizen"]}
