81e698bd010441de95218cb20b52b6a3e9b63abc
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-15 23:32:53 +0200

    Update home.py

--- home.py
+++ home.py
@@ -379,10 +379,14 @@ try:
         
     df_2["datum"] = pd.to_datetime(df_2["datum"]).dt.date
 
-    st.sidebar.subheader("Filter op",divider=False)
-    d = st.sidebar.slider("Datum", min_value=df_2.datum.min(),max_value=df_2.datum.max(),value=(df_2.datum.min(), df_2.datum.max()),format="DD-MM-YYYY")
-    
-    df_2 = df_2[(df_2['datum']>=d[0]) & (df_2['datum']<=d[1])]
+    try:
+        st.sidebar.subheader("Filter op",divider=False)
+        d = st.sidebar.slider("Datum", min_value=df_2.datum.min(),max_value=df_2.datum.max(),value=(df_2.datum.min(), df_2.datum.max()),format="DD-MM-YYYY")
+        
+        df_2 = df_2[(df_2['datum']>=d[0]) & (df_2['datum']<=d[1])]
+    except:
+        st.warning("Insert data!",icon="📌")
+        
     if st.session_state.project['opdracht'] in ["Vleermuizen","Vogels"]:
         species_filter_option = df_2["sp"].unique()
         species_filter = st.sidebar.multiselect("Sorten",species_filter_option,species_filter_option)
