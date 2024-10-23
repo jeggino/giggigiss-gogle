cf6c8a8c8d7b8c04bd96d79cb92090cf648e0708
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-23 10:51:54 +0200

    Update home.py

--- home.py
+++ home.py
@@ -83,5 +83,5 @@ if index_project == "No":
 else:
     disable = False
 
-st.sidebar.page_link("pages/home.py", label="Home", icon="🗺️", disabled=False)   
+st.sidebar.page_link("home.py", label="Home", icon="🗺️", disabled=False)   
 st.sidebar.page_link("pages/✍️_Voeg_een_waarneming_in.py", label="Voeg een waarneming in", icon="✍️", disabled=disable)
