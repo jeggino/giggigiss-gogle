71518f0ab208b80142b8518f8f86f76ad18140a2
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-23 10:23:35 +0200

    Update home.py

--- home.py
+++ home.py
@@ -96,7 +96,6 @@ create_page = st.Page("home.py", title="Home", icon=":material/add_circle:")
 delete_page = st.Page("pages/✍️_Voeg_een_waarneming_in.py", title="Entry", icon=":material/delete:")
 
 pg = st.navigation([create_page, delete_page])
-st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
 pg.run()
 
 # try:
