5fa669978bf170a38bae156bf9ff4e651664158f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-23 10:33:28 +0200

    Update home.py

--- home.py
+++ home.py
@@ -74,6 +74,6 @@ if 'project' not in st.session_state:
 
 
 
-index_project = df_references[df_references['username']==st.session_state.login["name"]]['allow'].values
+index_project = df_references[df_references['username']==st.session_state.login["name"]]#['allow'].values
 st.write(index_project)
 st.page_link("pages/✍️_Voeg_een_waarneming_in.py", label="Page 2", icon="2️⃣", disabled=index_project)
