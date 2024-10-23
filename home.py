92846b03650682d384b547703d504ee85bf77097
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-23 10:40:59 +0200

    Update home.py

--- home.py
+++ home.py
@@ -76,4 +76,5 @@ if 'project' not in st.session_state:
 
 index_project = df_references[df_references['username']==st.session_state.login["name"]]['allow'].values
 df_references
+df_references[df_references['username']==st.session_state.login["name"]]['allow'].values
 st.sidebar.page_link("http://www.google.com", label="Page 2", icon="2️⃣", disabled=False)
